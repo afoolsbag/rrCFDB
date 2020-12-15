9b324e1ecf9dfd6b83f8f859b4e08067ff220d22
afoolsbag <afoolsbag@gmail.com>
2020-12-15 11:22:01 +0800

    chore: 似乎发现一个 Ant Design Vue 2.0.0-rc.5 的 Bug，固定现场

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,81 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <a-config-provider :locale="locale">
+    <a-layout>
+      <a-layout-header class="header">
+        <img class="header__logo" alt="rrCFDB logo" src="@/assets/logo-250x65.png"
+             title="zhengrr's Crossfire Database"/>
+        <nav class="nav">
+          <a-menu class="nav__menu" mode="horizontal" theme="dark"
+                  v-model:selectedKeys="selectedKeys1"
+          >
+            <a-menu-item class="nav__menu-item" key="home">
+              <router-link to="/">Home</router-link>
+            </a-menu-item>
+            <a-menu-item clsss="nav__menu-item" key="about">
+              <router-link to="/about">About</router-link>
+            </a-menu-item>
+          </a-menu>
+        </nav>
+      </a-layout-header>
+      <a-layout-content clsss="content">
+        <router-view/>
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        Made with <a href="https://vuejs.org/">Vue.js</a> and <a href="https://antdv.com/">Ant Design Vue</a>.
+      </a-layout-footer>
+    </a-layout>
+  </a-config-provider>
 </template>
+
+<script lang="ts">
+import zh_CN from 'ant-design-vue/es/locale/zh_CN'
+
+export default {
+  data() {
+    return {
+      locale: zh_CN
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
+  // 字体
+  // https://developer.mozilla.org/docs/Web/CSS/font-family
   font-family: Avenir, Helvetica, Arial, sans-serif;
+
+  // 字体平滑
+  // https://developer.mozilla.org/docs/Web/CSS/font-smooth
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+.header {
+  &__logo {
+    // 浮动
+    // https://developer.mozilla.org/docs/Web/CSS/float
+    float: left;
+  }
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+.content, .ant-layout-content{
+  // 内边距区域
+  // https://developer.mozilla.org/docs/Web/CSS/padding
+  padding: 0 50px;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+.footer {
+  // 文字对齐
+  // https://developer.mozilla.org/docs/Web/CSS/text-align
+  text-align: center;
+}
+
+.nav {
+  &__menu {
+    // 行高
+    // https://developer.mozilla.org/docs/Web/CSS/line-height
+    line-height: 64px;
   }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,13 @@
+<!-- Vue 通过 Webpack 的 resolve 功能，将 @ 解析为 path.resolve('src') -->
+
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import {defineComponent} from 'vue'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
 
 export default defineComponent({
@@ -16,3 +17,7 @@ export default defineComponent({
   }
 })
 </script>
+
+<style lang="scss">
+
+</style>
